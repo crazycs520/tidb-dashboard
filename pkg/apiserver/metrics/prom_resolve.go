88836ae5baa220089b51ba50f4c6b06ad1bdda20
crazycs520 <crazycs520@gmail.com>
2025-07-06 14:47:12 +0800

    update prometheus http schema
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- pkg/apiserver/metrics/prom_resolve.go
+++ pkg/apiserver/metrics/prom_resolve.go
@@ -86,7 +86,7 @@ func (s *Service) resolveDeployedPromAddress() (string, error) {
 	if pi == nil {
 		return "", nil
 	}
-	return fmt.Sprintf("http://%s", net.JoinHostPort(pi.IP, strconv.Itoa(int(pi.Port)))), nil
+	return fmt.Sprintf("%s://%s", s.params.Config.GetClusterHTTPScheme(), net.JoinHostPort(pi.IP, strconv.Itoa(int(pi.Port)))), nil
 }
 
 // Resolve the final Prometheus address. When user has customized an address, this address is returned. Otherwise,
